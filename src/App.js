a87b9bda0dff955c7bdece7166144ef78637bb1b
unknown <cantorasupita@gmail.com>
2024-03-18 22:09:16 +0200

    1 commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let [items, setItem] = React.useState([])
+
+  const fetchData = () => {
+    fetch(`https://18.198.94.122/api/getCitiesWithTour`)
+      .then(res=>res.json())
+      .then(res=>{
+        console.log(res)
+        setItem(res.data)
+      })
+  }
+
+  React.useEffect(()=>{
+    fetchData()
+  },[])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          {items.map((item, id)=>{
+              return <div className='item' key={id}>
+                <img src={item.image}/>
+                {item.name}
+                </div>
+          })
+
+          }
       </header>
     </div>
   );
